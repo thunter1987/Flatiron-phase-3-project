473334fe649a67e6cbdfcfa17ec807d013b0a724
Tony <thunter1987@yahoo.com>
2023-08-19 14:46:28 -0500

    create models.py file

--- migrations/env.py
+++ migrations/env.py
@@ -15,9 +15,9 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
+from models import Base
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
