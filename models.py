c98a332c4e8f77f348dce19c5e3c168ade77a371
Tony Hunter <thunter1987@yahoo.com>
2023-08-21 17:15:02 -0500

    adds not null to seeds.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer,Float, String, Date
+from sqlalchemy import Column, Date, Integer, String, func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -6,9 +6,8 @@ Base = declarative_base()
 class Patient(Base):
     __tablename__ = "patient"
     
-    id = Column(Integer, primary_key=True)
-    patient_first_name = Column(String)
-    patient_last_name = Column(String)
-    patient_phone = Column(String)
-    patient_next_appointment = Column(Date)
-    patient_balance = Column(Float)
+    id = Column(Integer, primary_key=True, nullable=False)
+    patient_first_name = Column(String, nullable=False)
+    patient_last_name = Column(String, nullable=False)
+    patient_phone = Column(String, nullable=False)
+    patient_next_appointment = Column(Date(func.current_date))
