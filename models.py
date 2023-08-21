7d2153d7844672f23fec5743f1edbf3cc0b174b2
Tony <thunter1987@yahoo.com>
2023-08-21 00:15:58 -0500

    create patient_doctor.db in alembic

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer,Float, String, Date
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -9,4 +9,6 @@ class Patient(Base):
     id = Column(Integer, primary_key=True)
     patient_first_name = Column(String)
     patient_last_name = Column(String)
-pass
+    patient_phone = Column(String)
+    patient_next_appointment = Column(Date)
+    patient_balance = Column(Float)
