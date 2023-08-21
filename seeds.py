b6dbce716cadf3a4f38c5f091b0e917ef25993cf
Tony <thunter1987@yahoo.com>
2023-08-21 00:34:47 -0500

    adds seed.py file

---
+++ seeds.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from models import Patient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///patient_doctor.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.query(Patient).delete()
+
+patients = [
+    Patient(patient_first_name = "John", patient_last_name = "Smith"),
+    Patient(patient_first_name = "Jane", patient_last_name = "Doe", patient_phone = "832-287-9443", patient_next_appointment = datetime.now(), patient_balance = 125.97)
+]
