5a85d85d4afa08dd76ffbc1c9018a19ce68b1dcb
Tony Hunter <thunter1987@yahoo.com>
2023-08-22 11:34:42 -0500

    revision to patient table columns

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
-from DateTime import DateTime
-from sqlalchemy import Column, Integer, String, func
+from sqlalchemy import Column, Date, DateTime, Integer, String, func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -11,6 +10,7 @@ class Patient(Base):
     patient_first_name = Column(String, nullable=False)
     patient_last_name = Column(String, nullable=False)
     patient_phone = Column(String, nullable=False)
-    patient_next_appointment = Column(DateTime)
-    patient_created_date = Column(DateTime, server_default=func.current_timestamp())
-    patient_last_updated = Column(DateTime, onupdate=func.now())
+    patient_next_appointment = Column(DateTime, nullable = True)
+    patient_created_date = Column(Date, server_default=func.current_date())
+    patient_last_updated = Column(Date, onupdate=func.current_date())
+    
--- seeds.py
+++ seeds.py
@@ -11,12 +11,13 @@ session = Session()
 
 session.query(Patient).delete()
 
-patients = [
-    Patient(patient_first_name = "John", patient_last_name = "Smith"),
-    Patient(patient_first_name = "Jane", patient_last_name = "Doe", patient_phone = "832-287-9443", patient_next_appointment = datetime.now(), patient_balance = 125.97)
-]
 
-session.bulk_save_objects(patients)
+patients = [Patient(patient_first_name="John", patient_last_name="Smith", patient_phone="936-247-0238", patient_next_appointment=datetime(2023, 9, 2, 8, 30)),
+            Patient(patient_first_name="Jane", patient_last_name="Doe",
+                    patient_phone="832-287-9443", patient_next_appointment=datetime(2024, 8, 15, 13, 30))
+            ]
+session.add_all(patients)
 session.commit()
+session.close()
 
 print("Finished Seeding!")
