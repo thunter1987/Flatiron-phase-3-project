a243bb4e7b543714ac8684e9f1fe428afa64a3ee
Tony Hunter <thunter1987@yahoo.com>
2023-08-24 17:12:09 -0500

    formats seed.py next appointment to correct style

--- models.py
+++ models.py
@@ -1,27 +1,30 @@
-from sqlalchemy import Column, Date, DateTime, Integer, String, func
+from sqlalchemy import Column, DateTime, Integer, String, func
+from datetime import datetime
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class Patient(Base):
     __tablename__ = "patient"
-    
+
     id = Column(Integer, primary_key=True)
     patient_first_name = Column(String, nullable=False)
     patient_last_name = Column(String, nullable=False)
     patient_phone = Column(String, nullable=False)
-    patient_next_appointment = Column(DateTime)
-    patient_created_date = Column(Date, server_default=func.current_date())
-    patient_last_updated = Column(Date, onupdate=func.current_date())
-    
+    patient_next_appointment = Column(String(datetime) )
+    patient_created_date = Column(DateTime, server_default=func.current_date())
+    patient_last_updated = Column(DateTime, onupdate=func.current_date())
+
     def __repr__(self):
-        return f"\n<Patient " \
-        + f"id:{self.id}, " \
-        + f"first name:{self.patient_first_name}, " \
-        + f"last name:{self.patient_last_name}, " \
-        + f"phone:{self.patient_phone}, " \
-        + f"next appointment:{self.patient_next_appointment}, " \
-        + f"created on:{self.patient_created_date}, " \
-        + f"last updated on:{self.patient_last_updated}, " \
-        + " >"
-    
+        return (
+            f"\n<Patient "
+            + f"id:{self.id}, "
+            + f"first name:{self.patient_first_name}, "
+            + f"last name:{self.patient_last_name}, "
+            + f"phone:{self.patient_phone}, "
+            + f"next appointment:{self.patient_next_appointment}, "
+            + f"created on:{self.patient_created_date}, "
+            + f"last updated on:{self.patient_last_updated}, "
+            + " >"
+        )
--- seeds.py
+++ seeds.py
@@ -33,6 +33,19 @@ def generate_and_format_phone_number():
 
     # Generate and format a phone number until it's properly formatted
 properly_formatted_phone_number = generate_and_format_phone_number()
+
+def generate_formatted_datetime():
+
+    # Generate a random datetime within the specified range
+    random_datetime = fake.date_time_between_dates(datetime(2023, 8, 24, 8, 15), datetime(2024, 8, 23, 17, 0))
+
+    # Format the datetime as yyyy-mm-dd hh:mm
+    formatted_datetime = random_datetime.strftime("%Y-%m-%d %H:%M")
+    return formatted_datetime
+
+
+
+
     
 for _ in range(20):
 
@@ -40,13 +53,10 @@ for _ in range(20):
     patient = Patient(
         patient_first_name=fake.first_name(),
         patient_last_name=fake.last_name(),
-        patient_phone=generate_and_format_phone_number()
+        patient_phone=generate_and_format_phone_number(),
+        patient_next_appointment=generate_formatted_datetime()
     )
 
-    # patients = [Patient(patient_first_name="John", patient_last_name="Smith", patient_phone="936-247-0238", patient_next_appointment=datetime(year=2023, month=9, day=2, hour=8, minute=30)),
-    #             Patient(patient_first_name="Jane", patient_last_name="Doe",
-    #                     patient_phone="832-287-9443", patient_next_appointment=datetime(year=2024, month=8, day=15, hour=13, minute=30))
-    #             ]
     session.add(patient)
     print(patient)
 session.commit()
