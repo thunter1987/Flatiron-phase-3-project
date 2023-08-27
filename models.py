cdaa7e354f453e42f6f63d03eac36409bcad3ef7
Tony Hunter <thunter1987@yahoo.com>
2023-08-27 16:59:18 -0500

    corrects db errors

--- models.py
+++ models.py
@@ -36,6 +36,11 @@ class Doctor(Base):
     last_name = Column(String, nullable=False)
     specialty = Column(String)
     
+    def __init__(self, first_name, last_name, specialty):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.specialty = specialty
+    
     def __repr__(self):
         return (
             f"\n<Doctor "
--- seeds.py
+++ seeds.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Patient, Doctor
+import random
 
 print("Starting Seed")
 
@@ -19,56 +20,61 @@ fake = Faker()
 session.query(Patient).delete()
 session.query(Doctor).delete()
 
+
 def generate_and_format_phone_number():
-  while True:
-            
-    # Generate a random phone number as a string
-    phone_number = fake.phone_number()
+    while True:
+        # Generate a random phone number as a string
+        phone_number = fake.phone_number()
 
-    # Format the phone number as (XXX) XXX-XXXX using regular expressions
-    formatted_phone_number = re.sub(r"(\d{3})(\d{3})(\d{4})", r"(\1) \2-\3", phone_number)
+        # Format the phone number as (XXX) XXX-XXXX using regular expressions
+        formatted_phone_number = re.sub(
+            r"(\d{3})(\d{3})(\d{4})", r"(\1) \2-\3", phone_number
+        )
 
-    # Check if the formatted phone number matches the expected format
-    if re.match(r"\(\d{3}\) \d{3}-\d{4}", formatted_phone_number):
-      return formatted_phone_number
+        # Check if the formatted phone number matches the expected format
+        if re.match(r"\(\d{3}\) \d{3}-\d{4}", formatted_phone_number):
+            return formatted_phone_number
 
-    # Generate and format a phone number until it's properly formatted
+        # Generate and format a phone number until it's properly formatted
+
+
 properly_formatted_phone_number = generate_and_format_phone_number()
 
-def generate_formatted_datetime():
 
+def generate_formatted_datetime():
     # Generate a random datetime within the specified range
-    random_datetime = fake.date_time_between_dates(datetime(2023, 8, 24, 8, 15), datetime(2024, 8, 23, 17, 0))
+    random_datetime = fake.date_time_between_dates(
+        datetime(2023, 8, 24, 8, 15), datetime(2024, 8, 23, 17, 0)
+    )
 
     # Format the datetime as yyyy-mm-dd hh:mm
     formatted_datetime = random_datetime.strftime("%Y-%m-%d %H:%M")
     return formatted_datetime
 
 
-
-
-    
 for _ in range(20):
-
-
     patient = Patient(
-        patient_first_name=fake.first_name(),
-        patient_last_name=fake.last_name(),
-        patient_phone=generate_and_format_phone_number(),
-        patient_next_appointment=generate_formatted_datetime()
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+        phone=generate_and_format_phone_number(),
+        next_appointment=generate_formatted_datetime(),
     )
 
     session.add(patient)
     print(patient)
 
-for _ in range(5):
-  Doctor(self, first_name, last_name, specialty)
-  first_name=self.first_name,
-  last_name=self.last_name,
-  specialty=self.specialty
-  
-  session. add(doctor)
-  print(doctor)
+doctors = []
+specialties = ["Primary Care", "Cardiology", "Neurology", "Ophthalmology", "Pediatrics"]
+
+doctor1 = Doctor("Andrea", "Jones", "Ophthalmology")
+doctor2 = Doctor("Laura", "Richmond", "Cardiology")
+doctor3 = Doctor("Christopher", "Baker", "Neurology")
+doctor4 = Doctor("Thomas", "Walker", "Primary Care")
+doctor5 = Doctor("Joseph", "Smith", "Pediatrics")
+doctors = [doctor1, doctor2, doctor3, doctor4, doctor5]
+session.add_all(doctors)
+print(doctors)
 session.commit()
+session.close()
 
 print("Finished Seeding!")
