a75d008a99a466c06130b47596f2741df9b2a50b
Tony <thunter1987@yahoo.com>
2023-08-28 00:28:21 -0500

    creates cli opening menu

--- cli/cli.py
+++ cli/cli.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3
+
 from simple_term_menu import TerminalMenu
 
-def start():
-    options = ["Create new Patient", "Schedule Patient Next Appointment", "Exit" ]
-    terminal_menu = TerminalMenu(options)
-    menu_entry_index = terminal_menu.show()
-    print(f"{options[menu_entry_index]}")
+class Cli():
     
-if __name__ == '__main__':
-    start()
+    def start():
+      options = ["Create new Patient", "Schedule Patient Next Appointment", "Exit" ]
+      terminal_menu = TerminalMenu(options)
+      menu_entry_index = terminal_menu.show()
+      print(f"{options[menu_entry_index]}")
+      
+      if options[menu_entry_index] == "Exit":
+        print("Thank You for using this database, Goodbye!")
+        exit()
+        
+app=Cli
+app.start()
+    
