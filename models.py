9bd5b1403e0043f730abaee829dd15642a8af9a6
thunter1987 <116783221+thunter1987@users.noreply.github.com>
2023-08-31 06:06:38 -0500

    Update models.py

--- models.py
+++ models.py
@@ -54,4 +54,44 @@ class Doctor(Base):
             + f"specialty:{self.specialty}, "
             + " >"
         )
+        
+        
+        #########NEW CODE##########
+    
+
+'''
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = 'patients'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone_number = Column(String)
+    next_appointment = Column(String)
+    created_date = Column(String)
+    last_updated = Column(String)
+    appointments = relationship('Appointment', back_populates='patient')
+
+class Doctor(Base):
+    __tablename__ = 'doctors'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    appointments = relationship('Appointment', back_populates='doctor')
+
+class Appointment(Base):
+    __tablename__ = 'appointments'
+    id = Column(Integer, primary_key=True)
+    patient_id = Column(Integer, ForeignKey('patients.id'))
+    doctor_id = Column(Integer, ForeignKey('doctors.id'))
+    date_time = Column(String, nullable=False)
+    actual_doctor = Column(Boolean, default=False)
+    patient = relationship('Patient', back_populates='appointments')
+    doctor = relationship('Doctor', back_populates='appointments')
+    '''
     
